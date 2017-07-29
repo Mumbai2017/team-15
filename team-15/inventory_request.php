c1b352dfb366184de750c845603f00b63d59e447
Pratik Bhambhani <pratik110196@gmail.com>
2017-07-30 02:03:10 +0530

    final inventory request

--- team-15/inventory_request.php
+++ team-15/inventory_request.php
@@ -1,21 +1,23 @@
 <?php
-	$user_id=$_POST("");//$_SESSION("user_id");
-	$item_id=$_POST("");
-	$quantity=$_POST("");
+
+$servername = "localhost";
+$username = "root";
+$password = "pratik12";
+$dbname = "sanisa";
+
+	$user_id=$_POST["user_id"];//$_SESSION("user_id");
+	$item_id=$_POST["item_id"];
+	$quantity=$_POST["quantity"];
 	$booked_date=mkdate();
-	$status=$_POST("");
+	$status=$_POST["status"];
 	
-	$connection=mysqli_connnect("","","");
+	$connection=mysqli_connnect($servername, $username, $password, $dbname);
 	if(!$connection){
 		die("Data connection failed".mysqli_error($connection));
-	}
-	$select_db=mysqli_select_db('');
-	if(!$select_db){
-		die("Database connection failed".mysql_error($select_db));
-	}
-	
+	}	
 	
 	$query="insert into inventory_request values(".$user_id",".$item_id.",".$quantity.",".$booked_date.","$status");"
 
-
+	$query_run = mysqli_query($connection, $query);
+	
 ?>
