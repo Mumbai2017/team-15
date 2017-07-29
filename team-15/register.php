5493d74f12a31f26f2495a044a1afa4d49f6e608
yvedant <yadavvedant@gmail.com>
2017-07-29 16:05:45 +0530

    basic register.php ready (not tested)

--- team-15/register.php
+++ team-15/register.php
@@ -4,9 +4,7 @@
 	$name=$_POST("");
 	$email=$_POST("");
 	$address=$_POST("");
-	
-	
-
+	$online=$_POST("");
 	$connection=mysqli_connnect("","","");
 	if(!$connection){
 		die("Data connection failed".mysqli_error($connection));
@@ -15,5 +13,9 @@
 	if(!$select_db){
 		die("Database connection failed".mysql_error($select_db));
 	}
-
+	
+	$cnt = mysql_num_rows(mysql_query("SELECT * FROM TABLE"));
+	$query="insert into customer values(".$cnt.",".$name",".$email.",".$phone_no.","$address",".$online.");"
+	
+	
 ?>
