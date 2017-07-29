60736eb9613a9cb1ca58016b9b2a9113012ca10e
vaibhav1608 <vaibhav24_j@yahoo.co.in>
2017-07-29 21:44:57 +0530

    Ui changes for confirmation and feedback

---
+++ app/src/main/java/com/example/admin/cfg15/Feedback.java
@@ -0,0 +1,15 @@
+package com.example.admin.cfg15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.cfg15.R;
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
