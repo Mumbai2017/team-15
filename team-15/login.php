1e688b3bf85dfe84de0901a061bb467b56c80b5b
Pratik Bhambhani <pratik110196@gmail.com>
2017-07-29 17:33:54 +0530

    final login

--- team-15/login.php
+++ team-15/login.php
@@ -3,17 +3,34 @@
 $servername = "localhost";
 $username = "root";
 $password = "pratik12";
+$dbname = "demo";
 
-$conn = mysqli_connect($servername, $username, $password);
-
-$trial = file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBCyvmvjHJMyzuZf9oMLNrrQwnrqNgguKE");
-
-echo $trial;
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if(!$conn) {
 	die("Connection failed" . mysqli_error());
 }
-else {
-	echo "Connected successfully";
+// else {
+// 	echo "Connected successfully";
+// }
+;
+$email = $_POST["email"];
+$pwd = $_POST["pwd"];
+
+$get_password_query = "select password from trial where email = \"" . $email . "\"";
+
+$get_password = mysqli_query($conn, $get_password_query);
+if (mysqli_num_rows($get_password) > 0) {
+	$row = mysqli_fetch_assoc($get_password);
+	if($row["password"] == $pwd)
+	{
+		echo "authorised";
+	}
+	else {
+	echo "unauthorized";
+
 }
+
+}
+mysqli_close($conn);
 ?>
