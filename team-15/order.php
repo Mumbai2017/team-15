7c38f40841c4197285f6dbc97dead736d01c25c0
yvedant <yadavvedant@gmail.com>
2017-07-29 17:39:37 +0530

    order.php start

---
+++ team-15/order.php
@@ -0,0 +1,19 @@
+<?php
+
+	$lat_customer=$_POST("");
+	$long_customer=$_POST("");
+	
+	if(!$connection){
+		die("Data connection failed".mysqli_error($connection));
+	}
+	$select_db=mysqli_select_db('');
+	if(!$select_db){
+		die("Database connection failed".mysql_error($select_db));
+	}
+	
+	$q1="select "
+	$cnt = mysql_num_rows(mysql_query("SELECT * FROM TABLE"));
+	$query="insert into customer values(".$cnt.",".$name",".$email.",".$password.",".$phone_no.","$address",".$online.");"
+	
+	
+?>
