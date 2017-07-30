e5c2746966d6b63f8a410222659c601b1b8f1426
purva63 <yadavvedant@gmail.com>
2017-07-30 08:24:46 +0530

    added sakhi modules

--- sakhiApp/Sakhi/app/src/main/java/com/example/admin/sakhi/Orders.java
+++ sakhiApp/Sakhi/app/src/main/java/com/example/admin/sakhi/Orders.java
@@ -1,13 +1,15 @@
 package com.example.admin.sakhi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Orders extends AppCompatActivity {
-
+Intent in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+        Intent out=getIntent();
     }
 }
