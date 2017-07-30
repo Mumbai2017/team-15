b7ecb7b3ccce57faaad6758cad96e9ac36af7256
vaibhav1608 <vaibhav24_j@yahoo.co.in>
2017-07-30 06:25:25 +0530

    Resolved Error in layout files

--- sakhiApp/Sakhi/app/src/main/java/com/example/admin/sakhi/Registration.java
+++ sakhiApp/Sakhi/app/src/main/java/com/example/admin/sakhi/Registration.java
@@ -3,11 +3,12 @@ package com.example.admin.sakhi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Registeration extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registeration);
+        setContentView(R.layout.activity_registration);
     }
 }
