c821e631b543a74d7340b722110af798803d778b
Pratik Bhambhani <pratik110196@gmail.com>
2017-07-30 07:08:58 +0530

    get sakhi details

--- team-15/get_sakhi_details.php
+++ team-15/get_sakhi_details.php
@@ -12,7 +12,7 @@ $get_details_query = "select * from user where user_id = 1;";
 $run_query = mysqli_query($conn, $get_details_query);
 
 if(mysqli_num_rows($run_query) > 0) {
-	if ($result = mysqli_fetch_assoc($run_query) != null) {
+	while ($result = mysqli_fetch_assoc($run_query) != null) {
 		echo $result["user_id"];
 		echo $result["name"];
 		echo $result["email_id"];
